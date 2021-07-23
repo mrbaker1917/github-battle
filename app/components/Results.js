543de142e396ca37554528f8bbe93b4c3af97751
Mark R Baker <mrbaker1917@gmail.com>
2021-07-22 18:38:24 -0700

    adds Tooltip comp

--- app/components/Results.js
+++ app/components/Results.js
@@ -12,7 +12,7 @@ import {
 import Card from "./Card";
 import PropTypes from "prop-types";
 import Loading from "./Loading";
-import { render } from "react-dom";
+import Tooltip from "./tooltip";
 
 function ProfileList({ profile }) {
   return (
@@ -23,14 +23,18 @@ function ProfileList({ profile }) {
       </li>
       {profile.location && (
         <li>
-          <FaCompass color="rgb(144, 115, 255)" size={22} />
-          {profile.location}
+          <Tooltip text={`${profile.name}'s location`}>
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
+            {profile.location}
+          </Tooltip>
         </li>
       )}
       {profile.company && (
         <li>
-          <FaBriefcase color="#795548" size={22} />
-          {profile.company}
+          <Tooltip text={`${profile.name}'s company`}>
+            <FaBriefcase color="#795548" size={22} />
+            {profile.company}
+          </Tooltip>
         </li>
       )}
       <li>
--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -26,24 +26,36 @@ const styles = {
 export default class Tooltip extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      hovering: false,
+    };
 
     this.mouseOver = this.mouseOver.bind(this);
     this.mouseOut = this.mouseOut.bind(this);
   }
 
-  mouseOver(id) {
+  mouseOver() {
     this.setState({
-      [id]: true,
+      hovering: true,
     });
   }
 
-  mouseOut(id) {
+  mouseOut() {
     this.setState({
-      [id]: false,
+      hovering: false,
     });
   }
   render() {
-    return <div></div>;
+    const { text, children } = this.props;
+    const { hovering } = this.state;
+    return (
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} style={styles.container}>
+        {hovering === true && <div style={styles.tooltip}>{text}</div>}
+        {children}
+      </div>);
   }
+}
+
+Tooltip.propTypes = {
+  text: PropTypes.string.isRequired
 }
