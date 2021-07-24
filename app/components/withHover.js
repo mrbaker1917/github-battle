bdd5e005eed0ed00bf7cf2d83a756c74d432aac0
Mark R Baker <mrbaker1917@gmail.com>
2021-07-23 20:46:52 -0700

    adds HighOrdComp

---
+++ app/components/withHover.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function withHover(Component) {
+  return class WithHover extends React.Componen {
+    constructor(props) {
+      super(props);
+      this.state = {
+        hovering: false,
+      };
+
+      this.mouseOver = this.mouseOver.bind(this);
+      this.mouseOut = this.mouseOut.bind(this);
+    }
+
+    mouseOver() {
+      this.setState({
+        hovering: true,
+      });
+    }
+
+    mouseOut() {
+      this.setState({
+        hovering: false,
+      });
+    }
+
+    render() {
+      return (
+        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+          <Component hovering={this.state.hovering} {...this.props} />
+        </div>
+      )
+    }
+  };
+}
